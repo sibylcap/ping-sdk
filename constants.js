62f069a4d4377ff0a51b82be4f7744d2f4fc81be
Ubuntu <ubuntu@ip-172-31-90-250.ec2.internal>
2026-03-01 05:56:05 +0000

    fix: switch default RPC from mainnet.base.org to blastapi (503 reliability issues)

--- constants.js
+++ constants.js
@@ -2,7 +2,7 @@ export const CONTRACT = {
   address: '0xcd4af194dd8e79d26f9e7ccff8948e010a53d70a',
   deployBlock: 42772822n,
   erc8004Registry: '0x8004A169FB4a3325136EB29fA0ceB6D2e539a432',
-  rpc: 'https://mainnet.base.org',
+  rpc: 'https://base-mainnet.public.blastapi.io',
   chainId: 8453,
 };
 
